f0128ecbb8cd9fedd3a4091026b06afbd6bab177
kaliaaa1 <121272250+kaliaaa1@users.noreply.github.com>
2023-03-06 22:18:28 +0800

    Add files via upload

---
+++ sqlmap.sh
@@ -0,0 +1,24 @@
+#!/data/data/com.termux/files/usr/bin/bash
+apt update && apt upgrade -y > /dev/null 2>&1
+
+apt install git python openssl1.1-tool -y > /dev/null 2>&1
+
+echo "export LD_LIBRARY_PATH=/data/data/com.termux/files/usr/lib/openssl-1.1" >> ~/.bashrc
+
+if [ ! -d "sqlmapzh" ]; then
+    git clone --depth 1 https://github.com/kaliaaa1/sqlmapzh.git
+
+else
+
+fi 
+
+if [ -d "/data/data/com.termux/files/usr/bin/sqlmap" ]; then
+    chmod +x sqlmapzh/sqlmap.py
+
+    ln -s /data/data/com.termux/files/home/sqlmapzh/sqlmap.py /data/data/com.termux/files/usr/bin/
+
+    mv ../usr/bin/sqlmap.py ../usr/bin/sqlmap
+else
+
+fi 
+sqlmap
