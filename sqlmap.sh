9f288265e3c9dbcf5c332e9806706c30234314d3
kaliaaa1 <121272250+kaliaaa1@users.noreply.github.com>
2023-03-06 23:41:56 +0800

    Update sqlmap.sh

--- sqlmap.sh
+++ sqlmap.sh
@@ -5,20 +5,16 @@ apt install git python openssl1.1-tool -y > /dev/null 2>&1
 
 echo "export LD_LIBRARY_PATH=/data/data/com.termux/files/usr/lib/openssl-1.1" >> ~/.bashrc
 
-if [ ! -d "sqlmapzh" ]; then
+if [ ! -d "../sqlmapzh" ]; then
     git clone --depth 1 https://github.com/kaliaaa1/sqlmapzh.git
     
     mv Termux-install-sqlmap/sqlmapzh ../sqlmapzh
-
-else
 fi 
-if [! -s "/data/data/com.termux/files/usr/bin/sqlmap" ]; then
+if [ ! -s "/data/data/com.termux/files/usr/bin/sqlmap" ]; then
     chmod +x sqlmapzh/sqlmap.py
 
     ln -s /data/data/com.termux/files/home/sqlmapzh/sqlmap.py /data/data/com.termux/files/usr/bin/
 
     mv ../usr/bin/sqlmap.py ../usr/bin/sqlmap
-else
-
 fi 
 sqlmap
